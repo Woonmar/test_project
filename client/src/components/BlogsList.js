917fe5d11854790707af2d8e1cd3618996d4a35e
Woonmar <woonmar@example.com>
2021-01-30 23:03:39 +0700

    changes

--- client/src/components/BlogsList.js
+++ client/src/components/BlogsList.js
@@ -18,61 +18,61 @@ const BlogsList = () => {
     console.log('Data: ', data);
   },[data])
   
-  const deleteEvent = async (id) => {
-    await fetch(`api/blogs/${id}`, {
-      method: "DELETE",
-    })
-    setBlogs(blogs.filter((blog) => blog.id !== id)) 
-  }
+  // const deleteEvent = async (id) => {
+  //   await fetch(`api/blogs/${id}`, {
+  //     method: "DELETE",
+  //   })
+  //   setBlogs(blogs.filter((blog) => blog.id !== id)) 
+  // }
 
   const showForm = () => {
     setToggleForm(!toggleForm)
   }
 
-  const handleSubmit = (e) => { 
-    e.preventDefault();
-    const id = Math.floor(Math.random() * 100) + 1
-    const blog = {id, title, author };
+  // const handleSubmit = (e) => { 
+  //   e.preventDefault();
+  //   const id = Math.floor(Math.random() * 100) + 1
+  //   const blog = {id, title, author };
 
-    fetch('/api/blogs', {
-      method: 'POST', 
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify(blog)
-    }).then(() => {
-      console.log('New Blog Added');
-      setTitle('')
-      setAuthor('')
-      setBlogs([...blogs, blog])
-    })
-  }
+  //   fetch('/api/blogs', {
+  //     method: 'POST', 
+  //     headers: { "Content-Type": "application/json" },
+  //     body: JSON.stringify(blog)
+  //   }).then(() => {
+  //     console.log('New Blog Added');
+  //     setTitle('')
+  //     setAuthor('')
+  //     setBlogs([...blogs, blog])
+  //   })
+  // }
 
   const check = (id) => {
     console.log(`check box clicked ${id}`);
   }
 
-  const formfield = 
-    <div className="m-4">
-    <h2> Form </h2>
-    <form onSubmit={handleSubmit}>
-      <div className="form-group">
-      <label > Blog title : </label>
-      <input type="text" className="form-control" required value={title} onChange={(e) => setTitle(e.target.value)} />
-      </div>
-      <div className="form-group">
-      <label > Blog author : </label>
-      <input type="text" className="form-control" required value={author} onChange={(e) => setAuthor(e.target.value)} />
-      </div>
-      <button className="btn btn-success btn-block"> Add Blog</button>
-    </form>
-  </div>
+  // const formfield = 
+  //   <div className="m-4">
+  //   <h2> Form </h2>
+  //   <form onSubmit={handleSubmit}>
+  //     <div className="form-group">
+  //     <label > Blog title : </label>
+  //     <input type="text" className="form-control" required value={title} onChange={(e) => setTitle(e.target.value)} />
+  //     </div>
+  //     <div className="form-group">
+  //     <label > Blog author : </label>
+  //     <input type="text" className="form-control" required value={author} onChange={(e) => setAuthor(e.target.value)} />
+  //     </div>
+  //     <button className="btn btn-success btn-block"> Add Blog</button>
+  //   </form>
+  // </div>
 
     return ( 
       <div className="container">
-        <button className={toggleForm ? 'btn btn-secondary m-4':'btn btn-success m-4'} onClick={showForm}>
+        {/* <button className={toggleForm ? 'btn btn-secondary m-4':'btn btn-success m-4'} onClick={showForm}>
           {toggleForm ? 'Close Form' : 'Show Form'}
         </button>
         {toggleForm ? formfield : null}
-        {isLoading && <p>Loading...</p>}
+        {isLoading && <p>Loading...</p>} */}
         { blogs && blogs.length }
           {/* <table className="table">
             <thead className="thead-dark">
