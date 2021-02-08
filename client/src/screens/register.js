3d8c7309964ec357dd8bfa9e9fbd032e50885e3c
Woonmar <woonmar@example.com>
2021-02-08 15:30:44 +0700

    changes

---
+++ client/src/screens/register.js
@@ -0,0 +1,16 @@
+const Register = () => {
+  return ( 
+    <div>
+      <form action="">
+        <label htmlFor=""> Username </label>
+        <input type="text" />
+        
+        <label htmlFor="">Password</label>
+        <input type="password" />
+        <button className="btn btn-primary">Register</button>
+      </form>
+    </div>
+   );
+}
+ 
+export default Register;
